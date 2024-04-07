64397ce1bd6f9d79ddcdacac09e45441a99b4bd3
Manu Mohan T <manu.thekkedath@speridian.com>
2024-04-07 13:30:46 -0600

    fx

--- HDPredictor/Services/DatabaseService.cs
+++ HDPredictor/Services/DatabaseService.cs
@@ -4,7 +4,7 @@ namespace HDPredictor.Services
 {
     public class DatabaseService
     {
-        string connectionString = "Server=tcp:heartdisease.database.windows.net,1433;Initial Catalog=heartdisease;User ID=mobileuser;Password=Mobile@1234;MultipleActiveResultSets=False;TrustServerCertificate=True;Connection Timeout=30;";
+        //string connectionString = "Server=tcp:heartdisease.database.windows.net,1433;Initial Catalog=heartdisease;User ID=test;Password=test;MultipleActiveResultSets=False;TrustServerCertificate=True;Connection Timeout=30;";
         public DatabaseService() 
         {
 
