49e9aed2a01ebda3e40f5f9682f4cf17e4bcedc5
Manu Mohan T <manu.thekkedath@speridian.com>
2024-04-07 13:30:20 -0600

    fix

--- HDPredictor/Services/DatabaseService.cs
+++ HDPredictor/Services/DatabaseService.cs
@@ -11,19 +11,21 @@ namespace HDPredictor.Services
         }
         public async Task<bool> LoginAsync(string username, string password)
         { 
-            var found = false;
-            using(SqlConnection connection = new SqlConnection(connectionString))
-            {
-                await connection.OpenAsync();
-                SqlCommand comm = new Microsoft.Data.SqlClient.SqlCommand($"select username from login where username=@username and password=@password", connection);
-                comm.Parameters.AddWithValue("@username", username);
-                comm.Parameters.AddWithValue("@password", password);
-                var reader = await comm.ExecuteReaderAsync();
-                while (reader.Read())
-                {
-                    found = true;
-                }
-            }
+            var found = username == "Doctor" && password == "Doctor";
+            
+            //using(SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    await connection.OpenAsync();
+            //    SqlConnection.ClearPool(connection);
+            //    SqlCommand comm = new Microsoft.Data.SqlClient.SqlCommand($"select username from login where username=@username and password=@password", connection);
+            //    comm.Parameters.AddWithValue("@username", username);
+            //    comm.Parameters.AddWithValue("@password", password);
+            //    var reader = await comm.ExecuteReaderAsync();
+            //    while (reader.Read())
+            //    {
+            //        found = true;
+            //    }
+            //}
             
             return found;
         }
